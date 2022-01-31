a5ce923409b2ec9f6977c1455a05f145c463395f
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-01-31 17:39:30 +0100

    init Test

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    @Test
+    void init_test() {
+        Assertions.assertThat(false).isFalse();
+    }
+}
